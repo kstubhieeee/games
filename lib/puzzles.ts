42083df05dcb07ceb0ca0b3c23a03bb9c243493d
kstubhie <banekaustubh27@gmail.com>
2025-04-27 00:21:30 +0530

    Add new puzzle configurations to the puzzles array in puzzles.ts, introducing additional layouts to enhance gameplay variety and player engagement.

--- lib/puzzles.ts
+++ lib/puzzles.ts
@@ -52,7 +52,20 @@ export const puzzles: Dot[][] = [
   { id: 10, position: { row: 3, col: 1 } },
   { id: 11, position: { row: 4, col: 1 } },
   { id: 12, position: { row: 4, col: 2 } },
+  ],
+  [
+  { id: 1, position: { row: 0, col: 1 } },
+  { id: 2, position: { row: 4, col: 1 } },
+  { id: 3, position: { row: 5, col: 3 } },
+  { id: 4, position: { row: 2, col: 5 } },
+  { id: 5, position: { row: 1, col: 3 } },
+  { id: 6, position: { row: 0, col: 3 } },
+  { id: 7, position: { row: 2, col: 4 } },
+  { id: 8, position: { row: 4, col: 3 } },  // 8 is at col 2
+  { id: 9, position: { row: 3, col: 1 } },
+  { id: 10, position: { row: 1, col: 1 } },
 ]
+
 
 
 ]
